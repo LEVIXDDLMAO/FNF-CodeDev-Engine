40e4756946ec8aff4a534ffc61f3218fd5c3db9d
LEVIXDDLMAO <levi_xddd@outlook.com>
2022-11-11 05:13:54 +0000

    removed polymod since i dont really like it

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -63,9 +63,6 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
-		#if polymod
-		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod'], framework: OPENFL});
-		#end
 
 		FlxG.game.focusLostFramerate = 60;
 
